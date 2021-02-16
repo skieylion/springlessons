1628ca3eda37ffd686a33b1bbc678713711e495c
ubahhukob@yandex.ru <ubahhukob@ya.ru>
2021-02-16 23:55:56 +0300

    test commit 2

--- src/main/java/SpringGroup/App.java
+++ src/main/java/SpringGroup/App.java
@@ -31,7 +31,7 @@ public class App
     	//System.out.println(mp==mp2);
     	//System.out.println(mp);
     	//System.out.println(mp2);
-        //System.out.println(mp.getVolume());
+        //System.out.println(mp.getVolume()); 
         
         context.close();
         
